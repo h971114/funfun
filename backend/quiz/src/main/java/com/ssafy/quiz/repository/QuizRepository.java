68b353264ce1e913b87a761458a08e98cdae6dad
jinyoungHan <jyhan35@gmail.com>
2021-05-04 02:13:04 +0900

    퀴즈 서비스레이어 & 퀴즈 타입별 검색 추가

--- backend/quiz/src/main/java/com/ssafy/quiz/repository/QuizRepository.java
+++ backend/quiz/src/main/java/com/ssafy/quiz/repository/QuizRepository.java
@@ -1,9 +1,36 @@
 package com.ssafy.quiz.repository;
 
 import com.ssafy.quiz.domain.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
-public interface QuizRepository extends JpaRepository<Quiz, Integer> {
+@RequiredArgsConstructor
+@Transactional
+public class QuizRepository {
+
+    private final EntityManager em;
+
+    public void save(Quiz quiz){
+        em.persist(quiz);
+    }
+
+    public void delete(int quiz_no){
+        em.createQuery("delete from Quiz q where q.quiz_no = : quiz_no", Quiz.class)
+                .setParameter("quiz_no", quiz_no);
+    }
+
+    public Quiz find(int quiz_no){
+        return em.find(Quiz.class, quiz_no);
+    }
+
+    public List<Quiz> findByType(int type) {
+        return em.createQuery("select q from Quiz q where q.type = : type", Quiz.class)
+                .setParameter("type", type)
+                .getResultList();
+    }
 }
