6b5ba896b3ca4326356550b927fb09dba17b26e3
doggydeok2 <68405887+doggydeok2@users.noreply.github.com>
2021-05-10 17:11:17 +0900

    Add FindID n PW

--- frontend/funfun/src/routes/User/FindID.jsx
+++ frontend/funfun/src/routes/User/FindID.jsx
@@ -5,36 +5,23 @@ import axios from "axios";
 
 class FindID extends Component {
 
-    getUserByEmail = async (email) => {
-        const response = await axios.get(`http://127.0.0.1:8080/myapp/member/find-id`, {
-            params: {
-                email: email,
-            },
-        }).then(res => {
-            console.log(res)
-        });
-        return response.data;
-    }
-
     findID = () => {
-        let userInfo = this.getUserByEmail("doggydeok2@gmail.com")
-        console.log(userInfo)
-        // axios.get(`http://127.0.0.1:8080/myapp/member/find-id`, {
-        //     params: {
-        //         email: "doggydeok2@gmail.com",
-        //     },
-        //     }).then(res => {
-        //         console.log(res);
-        //         if (res.data.conclusion === "SUCCESS") {
-        //             // window.location.replace("/");
-        //         } else {
-        //             alert("정보와 일치하는 아이디가 존재하지 않습니다.")
-        //         }
-        //     }).catch(err => {
-        //         console.log(err);
-        //         alert("알 수 없는 오류가 발생했습니다.");
-        //         window.location.replace("/");
-        //     })
+        axios.get(`http://127.0.0.1:8080/myapp/member/find-id`, {
+            params: {
+                email: "doggydeok2@gmail.com",
+            },
+            }).then(res => {
+                // console.log(res);
+                if (res.data) {
+                    console.log(res.data)
+                } else {
+                    alert("정보와 일치하는 아이디가 존재하지 않습니다.")
+                }
+            }).catch(err => {
+                console.log(err);
+                alert("알 수 없는 오류가 발생했습니다.");
+                window.location.replace("/");
+            })
     }
 
     render () {
