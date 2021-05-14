89b33dec7656d895d4699e39309475d20973aa7c
doggydeok2 <68405887+doggydeok2@users.noreply.github.com>
2021-05-14 13:52:18 +0900

    Add MyQuiz 퀴즈 목록 호출

--- frontend/funfun/src/routes/User/MyQuiz.jsx
+++ frontend/funfun/src/routes/User/MyQuiz.jsx
@@ -8,31 +8,45 @@ class MyQuiz extends Component {
     constructor() {
         super();
         this.state = {
-            checkIDnEmail: false,
-            checkPW: false,
-            checkCPW: false,
             no: "",
             id: "",
             nick: "",
             email: "",
+            myRooms: [],
         }
     }
 
-    // getUserByID = (member_no) => {
-    //     axios.get(`http://127.0.0.1:8080/myapp/member/${member_no}`, {
-    //         params: {
-    //             no: member_no
-    //         }
-    //     }).then(res => {
-    //         console.log(res)
-    //         this.setState({
-    //             no: res.data.no,
-    //             id: res.data.id,
-    //             nick: res.data.nick,
-    //             email: res.data.email,
-    //         })
-    //     })
-    // }
+    getUserByID = (member_id) => {
+        axios.get(`http://127.0.0.1:8080/myapp/member/byid/${member_id}`, {
+            id: member_id
+        }).then(res => {
+            console.log(res)
+            this.setState({
+                no: res.data.member_no,
+                id: res.data.id,
+                nick: res.data.nick,
+                email: res.data.email,
+            })
+            this.getRoomsByMemberNo(res.data.member_no)
+        })
+    }
+
+    getRoomsByMemberNo (member_no) {
+        axios.get(`http://127.0.0.1:8080/myapp/room/room_memberno`, {
+            params: {
+                no: member_no,
+            }
+        }).then(res => {
+            console.log(res)
+            this.setState({ myRooms: res.data })
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    componentDidMount () {
+        this.getUserByID(sessionStorage.getItem('id'));
+    }
 
     render() {
         return (
@@ -74,62 +88,27 @@ class MyQuiz extends Component {
                         <div className="quizWrap">
                             <div className="quizList">
                                 <label className="scrollAlert">※ 스크롤하시면 리스트를 볼 수 있습니다.</label>
-                                <div className="quizLists">
-                                    <img src="./img/noImage.png" />
-                                    <div className="quizData">
-                                        <div className="quizTitle">
-                                            퀴즈 제목
+
+                                    {this.state.myRooms.map((room, index) => (
+                                        (
+                                        <div className="quizLists">
+                                            <img src="./img/noImage.png" />
+                                            <div className="quizData">
+                                                <div className="quizTitle">
+                                                    {index + 1}번째 퀴즈 목록
+                                                </div>
+                                                <div className="quizDetail">
+                                                    <span className="quizCnt">{room.quiz_cnt}문제 / </span>
+                                                    {/* <span className="quizDate">2021-05-12 /</span> */}
+                                                    <span className="quizTeam">{room.team_cnt}팀 / </span>
+                                                    <span className="quizRoomCode">{room.code} </span>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div className="quizDetail">
-                                            <span className="quizCnt">Q. 6문제 /</span>
-                                            <span className="quizDate">2021-05-12 /</span>
-                                            <span className="quizTeam">6 team /</span>
-                                            <span className="quizRoomCode">123 </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="quizLists">
-                                    <img src="./img/noImage.png" />
-                                    <div className="quizData">
-                                        <div className="quizTitle">
-                                            퀴즈 제목
-                                        </div>
-                                        <div className="quizDetail">
-                                            <span className="quizCnt">Q. 6문제 /</span>
-                                            <span className="quizDate">2021-05-12 /</span>
-                                            <span className="quizTeam">6 team /</span>
-                                            <span className="quizRoomCode">123 </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="quizLists">
-                                    <img src="./img/noImage.png" />
-                                    <div className="quizData">
-                                        <div className="quizTitle">
-                                            퀴즈 제목
-                                        </div>
-                                        <div className="quizDetail">
-                                            <span className="quizCnt">Q. 6문제 /</span>
-                                            <span className="quizDate">2021-05-12 /</span>
-                                            <span className="quizTeam">6 team /</span>
-                                            <span className="quizRoomCode">123 </span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="quizLists">
-                                    <img src="./img/noImage.png" />
-                                    <div className="quizData">
-                                        <div className="quizTitle">
-                                            퀴즈 제목
-                                        </div>
-                                        <div className="quizDetail">
-                                            <span className="quizCnt">Q. 6문제 /</span>
-                                            <span className="quizDate">2021-05-12 /</span>
-                                            <span className="quizTeam">6 team /</span>
-                                            <span className="quizRoomCode">123 </span>
-                                        </div>
-                                    </div>
-                                </div>
+                                        ))
+                                    )}
+
+
                                 <div className="quizLists">
                                     <img src="./img/noImage.png" />
                                     <div className="quizData">
@@ -156,7 +135,6 @@ class MyQuiz extends Component {
                         </div>
                     </div>
                 </div>
-
             </div>
         )
     }
