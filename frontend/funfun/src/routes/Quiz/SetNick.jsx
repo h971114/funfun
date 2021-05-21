72d8c95cbae002088e718af1d647ef34e6fdcc0d
doggydeok2 <68405887+doggydeok2@users.noreply.github.com>
2021-05-21 11:49:58 +0900

    Add checkNick

--- frontend/funfun/src/routes/Quiz/SetNick.jsx
+++ frontend/funfun/src/routes/Quiz/SetNick.jsx
@@ -5,54 +5,80 @@ import axios from 'axios';
 var nickname = ''
 var beforeid = ''
 var code = ''
+var checkNick = false
+
 class SetNick extends Component {
 
     goGame = (e) => {
-        if (cookie.load('ID') !== undefined) {
-            beforeid = cookie.load('ID');
-            axios.get(`${process.env.REACT_APP_SERVER_BASE_URL}/team/isbefore`, { params: { no: cookie.load('code'),  id : cookie.load('ID') } }).then(res => {
-                console.log(res);
-            });
-        }
-        code = cookie.load('code');
-        console.log(code);
-        if (nickname) {
-            cookie.save('ID', "", {
-                path: '/',
-                expires: new Date(Date.now()),
-            });
-            cookie.save('code', "", {
-                path: '/',
-                expires: new Date(Date.now()),
-            });
-            cookie.save('nickname', "", {
-                path: '/',
-                expires: new Date(Date.now()),
-            });
-            cookie.save('beforeid', beforeid,{
-                path: '/',
-                expires: new Date(Date.now())
-            })
-            this.props.history.push({
-                pathname: '/game/PlayQuiz',
-                state: {
-                    nickname: nickname,
-                    code: code,
-                }
-            });
-            console.log(this.props.history);
+        if (checkNick) {
+            if (cookie.load('ID') !== undefined) {
+                beforeid = cookie.load('ID');
+                axios.get(`${process.env.REACT_APP_SERVER_BASE_URL}/team/isbefore`, { params: { no: cookie.load('code'),  id : cookie.load('ID') } }).then(res => {
+                    console.log(res);
+                });
+            }
+            code = cookie.load('code');
+            console.log(code);
+            if (nickname) {
+                cookie.save('ID', "", {
+                    path: '/',
+                    expires: new Date(Date.now()),
+                });
+                cookie.save('code', "", {
+                    path: '/',
+                    expires: new Date(Date.now()),
+                });
+                cookie.save('nickname', "", {
+                    path: '/',
+                    expires: new Date(Date.now()),
+                });
+                cookie.save('beforeid', beforeid,{
+                    path: '/',
+                    expires: new Date(Date.now())
+                })
+                this.props.history.push({
+                    pathname: '/game/PlayQuiz',
+                    state: {
+                        nickname: nickname,
+                        code: code,
+                    }
+                });
+                console.log(this.props.history);
+            } else {
+                alert("닉네임을 확인해 주세요.")
+            }
         } else {
-            alert("닉네임을 입력하세요.")
+            alert("닉네임을 확인해 주세요.")
         }
     }
     changenick = (e) => {
         nickname = e;
+        this.checkNick();
     }
+
+    checkNick = () => {
+        var nick = document.getElementById('inputNick').value;
+        if (!nick) {
+            this.setState({ checkNick: false })
+            document.getElementById("avalidNN").setAttribute('style', 'color:#f91c37');
+            document.getElementById("avalidNN").innerText = "닉네임을 입력해주세요.";
+        }
+        else if (nick.length > 10) {
+                this.setState({ checkNick: false })
+                document.getElementById("avalidNN").setAttribute('style', 'color:#f91c37');
+                document.getElementById("avalidNN").innerText = "닉네임은 최대 10글자까지 가능합니다.";
+            } else {
+            this.setState({ checkNick: true })
+            document.getElementById("avalidNN").style.display = 'none';
+        }
+    }
+
     render() {
         return (
             <div className="quiz_contents nickbg">
-                <input type="text" className="code" onChange={event => this.changenick(event.target.value)}></input>
+                <input id="inputNick" type="text" className="code" onChange={event => this.changenick(event.target.value)}></input>
                 <input type="button" value="접속" className="codebtn" onClick={this.goGame}></input>
+                <div className="availd" id="avalidNN"></div>
                 {/* <div className="input_code">
                     <h2>닉네임을 입력하세요.</h2>
                 </div> */}
